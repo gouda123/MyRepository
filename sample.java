5aa6c9dbcebbe8db32e0335e91462e8feb287de9
gouda123 <32894594+gouda123@users.noreply.github.com>
2019-05-22 18:21:43 +0530

    sample.java
    Simple java file

---
+++ sample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Simple {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.println("enter the first number to add:");
+int a=sc.nextInt();
+System.out.println("enter the first number to add:");
+int b=sc.nextInt();
+
+int c = a+b;
+System.out.println("Addition of two numbers:"+c);
+}
+}
